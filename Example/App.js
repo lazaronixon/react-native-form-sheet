998fdf9e2717c6e3ab210832fe90c0e308340120
Lázaro Nixon <lazaronixon@hotmail.com>
2018-03-03 00:23:31 -0300

    link libraries

--- Example/App.js
+++ Example/App.js
@@ -1,38 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, Text, View, StyleSheet, Button } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
+  showFormSheet = () => {
+    alert('formsheet')
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button onPress={this.showFormSheet} title="Show FormSheet" />
       </View>
     );
   }
@@ -49,10 +30,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
--- ios/RNFormSheet.m
+++ ios/RNFormSheet.m
@@ -3,11 +3,11 @@
 
 @implementation RNFormSheet
 
-- (dispatch_queue_t)methodQueue
-{
+RCT_EXPORT_MODULE()
+
+- (dispatch_queue_t)methodQueue {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
 
 @end
   
