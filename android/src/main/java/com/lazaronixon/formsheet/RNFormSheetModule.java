cdc065bd5924c057555960e6e7b4bb7f415f10b4
Lázaro Nixon <lazaronixon@hotmail.com>
2018-03-06 15:00:36 -0300

    Rename package android

--- android/src/main/java/com/lazaronixon/formsheet/RNFormSheetModule.java
+++ android/src/main/java/com/lazaronixon/formsheet/RNFormSheetModule.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.lazaronixon.formsheet;
 
 import android.app.Activity;
 import android.os.Bundle;
--- android/src/main/java/com/lazaronixon/formsheet/RNFormSheetPackage.java
+++ android/src/main/java/com/lazaronixon/formsheet/RNFormSheetPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.lazaronixon.formsheet;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -9,7 +9,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNFormSheetPackage implements ReactPackage {
     @Override
